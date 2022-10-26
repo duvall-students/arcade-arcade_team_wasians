8bc44d4cd742edd9326881940191c1dc7b63bc62
brandon <xyan@elon.edu>
2022-10-26 17:27:17 -0400

    Update Necessary Java Files to Project
    1). Transit to new package system.
    2). Add the game elements.
    3). Modification of class names.
    4). Fixed some bugs (caused by implementing the packages system).
    5). Add header comments in each class so it shows the description and
    author of it.
    6). Still have several bugs that need to fix.

---
+++ src/gamePlaySystem/Player.java
@@ -0,0 +1,53 @@
+package gamePlaySystem;
+
+import gamePlaySystem.LevelSystem.GameLevel;
+
+/**
+ * @author chris lee
+ * handles Player information such as health, score, and whether the player is ready to play. 
+ */
+
+
+public class Player {
+	private int health;
+	private int score;
+	private boolean isReadyToPlay;
+	
+	public Player(int health) {
+		this.health = health;
+		this.score = 0;
+		isReadyToPlay = false;
+	}
+	
+	public void setReadytoPlay(boolean status) {
+		isReadyToPlay = status;
+	}
+	
+	public boolean isPlayerReady() {
+		return isReadyToPlay;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void rechargeHealth(GameLevel level) {
+		health = level.getAllowedHealth();
+	}
+	
+	public boolean isPlayerDead() {
+		return health<=0;
+	}
+	
+	public void playerLoseHealth() {
+		health--;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void addScore(int scoreAmt) {
+		score += scoreAmt;
+	}
+}
---
+++ src/games/Galaga.java
@@ -0,0 +1,13 @@
+package games;
+
+/**
+ * @Author: Hunter Copeland
+ */
+
+public class Galaga extends Game{
+
+	public void runGalaga() {
+		launch();
+	}
+	
+}
