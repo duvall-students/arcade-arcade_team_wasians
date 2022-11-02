f10d5cced3cbd2e74bd029e4859932645bf2b4fe
christopher-c-lee <82339562+christopher-c-lee@users.noreply.github.com>
2022-11-01 21:55:49 -0400

    player implements maxScore

--- src/gamePlaySystem/Player.java
+++ src/gamePlaySystem/Player.java
@@ -2,7 +2,15 @@ package gamePlaySystem;
 
 import gamePlaySystem.LevelSystem.GameLevel;
 import javafx.scene.text.Text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import gamePlaySystem.PlayerMessaging;
+
 
 /**
  * @author chris lee
@@ -14,6 +22,7 @@ public class Player {
 	private int health;
 	private int score;
 	private boolean isReadyToPlay;
+	public static String MAX_SCORE_FILE = "resources/maxScore.txt";
 	
 	public Player(int health) {
 		this.health = health;
@@ -52,5 +61,53 @@ public class Player {
 	
 	public void addScore(int scoreAmt) {
 		score += scoreAmt;
+	}
+	
+	public int getMaxScore() {
+		File maxData = new File(MAX_SCORE_FILE);
+		try{
+			Scanner reader = new Scanner(maxData);
+			if (reader.hasNextLine()) {
+				return reader.nextInt();
+			}
+			reader.close();
+		}
+		catch (FileNotFoundException e) {
+		      System.out.println("No Such File Found");
+		      e.printStackTrace();
+		}
+		
+		return 0;
+	}
+	
+	public void updateMaxScore() {
+		if (getMaxScore() > score){
+			File originalFile = new File(MAX_SCORE_FILE);
+			File tempFile = new File("tempfile.txt");
+	        try {
+				PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+				pw.println(score);
+				
+				if (!new File(MAX_SCORE_FILE).delete()) {
+		            System.out.println("Could not delete file");
+		            return;
+		        }
+
+		        // Rename the new file to the filename the original file had.
+		        if (!tempFile.renameTo(originalFile)) {
+		            System.out.println("Could not rename file");
+		        }
+		        
+			} catch (IOException e) {
+				System.out.println("couln't write on the file.");
+				e.printStackTrace();
+			}
+	       
+		}
+		
+		
+		
+        
+        
 	}
 }
